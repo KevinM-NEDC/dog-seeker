994a7d390c89ea2cd173431755718a4bfd6a79d6
Alex <18anguyen9@gmail.com>
2022-07-13 21:58:54 +0000

    site v2

--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ from flask import render_template
 from url_utils import get_base_url
 import os
 import torch
+import json
+import pandas as pd
 
 # setup the webserver
 # port may need to be changed if there are multiple flask servers running on same server
@@ -18,13 +20,17 @@ else:
     app = Flask(__name__, static_url_path=base_url+'static')
 
 UPLOAD_FOLDER = 'static/uploads'
+RESULTS_FOLDER = 'static/results'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+dummy_json = pd.read_json('File-1.json').to_dict()
+
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['RESULTS_FOLDER'] = RESULTS_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 32 * 1024 * 1024
 
 model = torch.hub.load("ultralytics/yolov5", "custom", path = 'best4.pt', force_reload=True)
-#model.conf = 0.6
+model.conf = 0.6
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -48,14 +54,13 @@ def home():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
+            return redirect(url_for('uploaded_file', filename=filename))
 
     return render_template('home.html')
 
 
 @app.route(f'{base_url}/uploads/<filename>', methods=['GET', 'POST'])
-def uploaded_file(filename):
+def uploaded_file(filename): #
     if request.method == 'POST':
         if 'file' not in request.files:
             flash('No file part')
@@ -70,13 +75,17 @@ def uploaded_file(filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('uploaded_file', filename=filename))
+
     here = os.getcwd()
     image_path = os.path.join(here, app.config['UPLOAD_FOLDER'], filename)
     results = model(image_path, size=416)
+
     if len(results.pandas().xyxy[0]) >  0:
         results.print()
-        save_dir = os.path.join(here, app.config['UPLOAD_FOLDER'])
-        results.save(save_dir=save_dir)
+        result_dir = os.path.join(here, app.config['RESULTS_FOLDER'])
+        results.save(save_dir=result_dir)
+        result_filename = filename.split('.')[0]
+        result_filename += ".jpg"
         def and_syntax(alist):
             if len(alist) == 1:
                 alist = "".join(alist)
@@ -98,17 +107,37 @@ def uploaded_file(filename):
         format_confidences = and_syntax(format_confidences)
 
         labels = list(results.pandas().xyxy[0]['name'])
-        # labels: sorting and capitalizing, putting into function
-        labels = set(labels)
-        labels = [emotion.capitalize() for emotion in labels]
+        #checks repeats
+        label_reps=[]
+        for i in labels:
+            if i not in label_reps:
+                label_reps.append(i)
+            
+        labels = label_reps
+        
+        for i in range(len(labels)): #renames to proper names
+            if labels[i] == 'Shibu Inu':
+                labels[i] = 'Shiba Inu'
+            elif labels[i] == "Akita":
+                labels[i] = "Akita Inu"
+        dog_Names= labels
+        dog_Info= dummy_json[dog_Names[0]]['Personality']
+        dog_desc= dummy_json[dog_Names[0]]['Description']
         labels = and_syntax(labels)
-        return render_template('results-example.html', confidences=format_confidences, labels=labels,
+        # labels: sorting and capitalizing, putting into function
+        return render_template('results.html', confidences=format_confidences, labels=labels,
                                old_filename=filename,
-                               filename=filename)
+                               filename=result_filename,
+                               dog_Names = dog_Names, #Do we need dog names? We could just replace each mention of dog_Names with labels 
+                              dog_Personality=dog_Info,
+                              dog_Description=dog_desc)
 
     else:
-        return render_template('results-example.html', labels='no dogs', old_filename=filename, filename=filename)
+        return render_template('results.html', labels='no dogs', old_filename=filename, filename=filename)
 
+@app.route(f'{base_url}/test', methods=['GET', 'POST'])
+def test():
+    return render_template('index.html')
 
 @app.route(f'{base_url}/uploads/<path:filename>')
 def files(filename):
@@ -119,27 +148,6 @@ def files(filename):
 # @app.route(f'{base_url}/team_members')
 # def team_members():
 #     return render_template('team_members.html') # would need to actually make this page
-
-@app.route(f'{base_url}/test', methods=['GET', 'POST'])
-def test():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file', filename=filename))
-    return render_template('test.html')
 
 if __name__ == '__main__':
     # IMPORTANT: change url to the site where you are editing this file.
